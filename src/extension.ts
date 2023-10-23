3895458ad2f953ad572ae7f0459057d9f03b95f9
marxgeek <mgangbala610@gmail.com>
2023-10-23 20:34:34 +0100

    feat: alphabetical sorting of css properties (wip)

--- src/extension.ts
+++ src/extension.ts
@@ -7,42 +7,54 @@ async function sortCSSProperties(editor: vscode.TextEditor | undefined) {
 		return;
 	}
 
+	const textEditor = vscode.window.activeTextEditor; // activé l'accès au méthode de l'éditeur 
+	const document = textEditor?.document; // accès au fichier
+
 	const fileDocument = editor.document; //accéder au contenu de l'éditeur
-	const textStyle = fileDocument.getText(); // récupéré les text
+	const textStyle = fileDocument.getText(); // récupéré le texte du fichier
+	let sortedText:string | undefined = "";
+	let formattedText:string = "";
 
-	const cssBlocks = textStyle.match(/[^{]*{[^}]*}/gs);
+	if (document?.languageId === "scss") {
+		console.log("scss");
+	} else if (document?.languageId === "css") {
+		const cssBlocks = textStyle.match(/(?<={)(.*?)(?=\})/gs);
 
-	if (!cssBlocks) {
-	  return;
+		if (!cssBlocks) {
+		return;
+		}
+
+		const sortedBlocks = cssBlocks.map((block) => {
+			// trie en ordre alphabétique
+			const lines = block
+				.split('\n')
+				.sort((a, b) => {
+					const propertyA = a.split(':')[0];
+					const propertyB = b.split(':')[0];
+					return propertyA.localeCompare(propertyB);
+				});
+
+			return lines.join('\n');
+		});
+
+		// reconstruire le texte
+		sortedText = textStyle;
+		for (let i = 0; i < cssBlocks.length; i++) {
+			sortedText = sortedText.replace(cssBlocks[i], sortedBlocks[i]);
+		}
 	}
 
-	const sortedBlocks = cssBlocks.map((block) => {
-		// trie en ordre alphabétique
-		const lines = block
-			.split('\n')
-			.sort((a, b) => {
-				const propertyA = a.split(':')[0];
-				const propertyB = b.split(':')[0];
-				return propertyA.localeCompare(propertyB);
-			});
-
-		return lines.join('\n');
-	});
-
-	// reconstruire le texte
-	let sortedText = textStyle;
-	for (let i = 0; i < cssBlocks.length; i++) {
-		sortedText = sortedText.replace(cssBlocks[i], sortedBlocks[i]);
-	}
-
-	const formattedText = await prettier.format(sortedText, { parser: 'css' });
-
+	formattedText = await prettier.format(sortedText, { parser: 'css' });
 	editor.edit((editBuilder) => {
 		editBuilder.replace(new vscode.Range(fileDocument.positionAt(0), fileDocument.positionAt(textStyle.length)), formattedText);
 	})
-}
+} 
+
 
 export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "css/scss-formatter" is now active!');
+	
 	const disposable = vscode.commands.registerCommand('extension.sortCSSProperties', () => {
 		const editor = vscode.window.activeTextEditor;
 		sortCSSProperties(editor);
